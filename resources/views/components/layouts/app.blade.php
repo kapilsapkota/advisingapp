ea960e04120bc846e49d3b7351f199ae784e74dd
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-07-25 21:07:54 -0400

    Reinstall
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,20 +1,28 @@
 <!DOCTYPE html>
-<html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8" />
 
-<head>
-    <meta charset="utf-8" />
-    <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1"
-    />
-    @vite(['resources/css/app.css'])
-    <title>{{ __('panel.site_title') }}</title>
-</head>
+        <meta name="application-name" content="{{ config('app.name') }}" />
+        <meta name="csrf-token" content="{{ csrf_token() }}" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-<body class="bg-blueGray-800 text-blueGray-700 antialiased">
-    <main>
-        @yield('content')
-    </main>
-</body>
+        <title>{{ config('app.name') }}</title>
 
+        <style>
+            [x-cloak] {
+                display: none !important;
+            }
+        </style>
+
+        @filamentStyles
+        @vite('resources/css/app.css')
+    </head>
+
+    <body class="antialiased">
+        {{ $slot }}
+
+        @filamentScripts
+        @vite('resources/js/app.js')
+    </body>
 </html>
